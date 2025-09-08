da5df2517183b54d8bc32b715aa286165b580256
Raycx86 <arthurdrouin8@gmail.com>
2025-09-08 14:10:52 +0200

    First real commit : -Added driver search

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 import requests
-import os
 
 api_link = "https://f1api.dev/api/"
+
+
 def heartbeat():
-    response = requests.get(f'{api_link}heartbeat')
+    response = requests.get(f"{api_link}heartbeat")
     print(f"Heartbeat : {response.status_code} - {response.text}")
 
-def get_driver(driver):
-    response = requests.get(f'{api_link}drivers/search?q={driver}')
+
+def get_driverid(driver):
+    response = requests.get(f"{api_link}drivers/search?q={driver}")
+
+    print(response.status_code)
     if response.status_code == 200:
-        print("Pilotes :", response.json())
+        data = response.json()
+        print(data)
+        # driverid = data.get("drivers").get("driverId", "Pas de driverId")
+        driverid = data.get("drivers", [{}])[0].get("driverId", "Pas de driverId")
+        print(f"DriverId trouvé : {driverid}")
+        return driverid
     else:
         print(f"Erreur lors de la récupération des pilotes ({response.status_code})")
         print(response.text)
+        return None
+
+
+def get_result(driver_result):
+    print(f"{api_link}{driver_result}/drivers/{driver}")
+    response = requests.get(f"{api_link}{driver_result}/drivers/{driver}")
+    print(response.json())
 
 
 # Utilisation
 heartbeat()
-driver_input = input("Quel est le pilot ? ")
-get_driver(driver_input)
+driver_input = input("Quel est le pilote ? ")
+driver = get_driverid(driver_input)
+searched_year = input("Resultat de quelle année ? ")
+driver_result = get_result(searched_year)
